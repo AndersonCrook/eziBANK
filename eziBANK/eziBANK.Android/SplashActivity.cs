1501ee69ef70d38268053bd47de340f6cbfe157c
Anderson Oliveira <anderson@MacBook-Pro-de-foursys.local>
2019-03-13 15:19:40 -0300

    icon android

---
+++ eziBANK/eziBANK.Android/SplashActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Support.V7.App;
+using eziBANK.Droid;
+
+namespace ProjetoVagas.Droid
+{
+    [Activity(Label = "eziBANK", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    class SplashActivity : AppCompatActivity
+    {
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
